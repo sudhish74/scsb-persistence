78b868895acd7e8263f9ada54a94fbdc3738fbf3
Peri Health <info@perihealth.com>
2016-06-11 09:06:54 -0400

    Refactored package and class names; Added Rest Controller for Bib.

---
+++ src/main/java/org/recap/controller/BibController.java
@@ -0,0 +1,33 @@
+package org.recap.controller;
+
+import org.recap.model.BibliographictEntity;
+import org.recap.repository.BibDetailsRespository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by pvsubrah on 6/11/16.
+ */
+
+@RestController
+@RequestMapping("/bib")
+public class BibController {
+    private final BibDetailsRespository bibDetailsRespository;
+
+    @Autowired
+    public BibController(BibDetailsRespository bibDetailsRespository) {
+        this.bibDetailsRespository = bibDetailsRespository;
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    public BibliographictEntity findOne(Long bibId) {
+        return bibDetailsRespository.findOne(bibId);
+    }
+    @RequestMapping(method = RequestMethod.POST)
+    public BibliographictEntity create(@RequestBody BibliographictEntity bibliographictEntity) {
+        return bibDetailsRespository.save(bibliographictEntity);
+    }
+}
