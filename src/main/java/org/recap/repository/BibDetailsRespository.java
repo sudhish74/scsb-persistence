44fdf7a5661e8f98410520e92426f30121d2c6ab
Peri Health <info@perihealth.com>
2016-06-11 09:08:08 -0400

    Added Main class.

---
+++ src/main/java/org/recap/repository/BibDetailsRespository.java
@@ -0,0 +1,9 @@
+package org.recap.repository;
+
+import org.recap.model.BibliographictEntity;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Created by pvsubrah on 6/10/16.
+ */
+public interface BibDetailsRespository extends CrudRepository<BibliographictEntity, Long> {}
