4d23273c68d979d4beffe5526b814286fa8d44e8
Peri Health <info@perihealth.com>
2016-06-17 09:50:46 -0400

    Added pageable request.

--- src/main/java/org/recap/repository/BibliographicDetailsRepository.java
+++ src/main/java/org/recap/repository/BibliographicDetailsRepository.java
@@ -2,8 +2,9 @@ package org.recap.repository;
 
 import org.recap.model.BibliographicEntity;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 /**
  * Created by pvsubrah on 6/10/16.
  */
-public interface BibliographicDetailsRepository extends CrudRepository<BibliographicEntity, Integer> {}
+public interface BibliographicDetailsRepository extends PagingAndSortingRepository<BibliographicEntity, Integer> {}
--- src/test/java/org/recap/model/BibliographicEntity_Test.java
+++ src/test/java/org/recap/model/BibliographicEntity_Test.java
@@ -4,12 +4,11 @@ import org.junit.Test;
 import org.recap.BaseTestCase;
 import org.recap.repository.BibliographicDetailsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.util.StopWatch;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Date;
-import java.util.List;
+import java.util.*;
 
 import static org.junit.Assert.assertNotNull;
 
@@ -37,13 +36,33 @@ public class BibliographicEntity_Test extends BaseTestCase {
     }
 
     @Test
-    public void findAll() throws Exception{
+    public void findAll() throws Exception {
         StopWatch stopWatch = new StopWatch();
         stopWatch.start();
         List<BibliographicEntity> bibliographicEntityList = new ArrayList<>();
         bibliographicEntityList.addAll((Collection<? extends BibliographicEntity>) bibliographicDetailsRepository.findAll());
         stopWatch.stop();
         System.out.println("Total time for " + bibliographicEntityList.size() + " - bibs : " + stopWatch.getTotalTimeSeconds() + "ms");
+    }
+
+
+    @Test
+    public void findAllByPage() throws Exception {
+        Page<BibliographicEntity> bibliographicEntityPage = bibliographicDetailsRepository.findAll(new PageRequest(0, 10));
+
+        Iterator<BibliographicEntity> iterator = bibliographicEntityPage.iterator();
+        while(iterator.hasNext()){
+            BibliographicEntity bibEntity = iterator.next();
+            System.out.println(bibEntity.getBibliographicId());
+        }
+
+        Page<BibliographicEntity> bibliographicEntityPage1 = bibliographicDetailsRepository.findAll(new PageRequest(1, 10));
+
+        Iterator<BibliographicEntity> iterator1 = bibliographicEntityPage1.iterator();
+        while(iterator1.hasNext()){
+            BibliographicEntity bibEntity = iterator1.next();
+            System.out.println(bibEntity.getBibliographicId());
+        }
     }
 
 }
